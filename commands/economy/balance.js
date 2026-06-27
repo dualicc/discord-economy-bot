0ed9a850dff17e257e2102e38db13aead95bd48d
dualic <officialdualic@gmail.com>
2026-06-27 14:38:22 -0400

    Update balance.js

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -1,35 +1,52 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const {
+    SlashCommandBuilder,
+    EmbedBuilder
+} = require("discord.js");
+
 const { getUser } = require("../../database/database");
 
 module.exports = {
+
     data: new SlashCommandBuilder()
         .setName("balance")
-        .setDescription("View your current balance.")
+        .setDescription("View your balance.")
         .addUserOption(option =>
             option
                 .setName("user")
-                .setDescription("The user to check.")
-                .setRequired(false)
+                .setDescription("User to check")
         ),
 
     async execute(interaction) {
 
         const target =
-            interaction.options.getUser("user") || interaction.user;
+            interaction.options.getUser("user") ||
+            interaction.user;
 
         const user = getUser(target.id);
 
         const embed = new EmbedBuilder()
-            .setColor(0x2b2d31)
+            .setColor(0x57F287)
             .setTitle("💰 Balance")
-            .setDescription(
-                `**${target.username}** currently has **${user.balance.toLocaleString()} Coins**.`
+            .addFields(
+                {
+                    name: "Wallet",
+                    value: `${user.balance.toLocaleString()} Coins`,
+                    inline: true
+                },
+                {
+                    name: "Bank",
+                    value: `${user.bank.toLocaleString()} Coins`,
+                    inline: true
+                }
             )
-            .setTimestamp();
+            .setFooter({
+                text: target.username
+            });
 
         await interaction.reply({
             embeds: [embed]
         });
 
     }
+
 };
