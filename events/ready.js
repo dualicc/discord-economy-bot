1f1769b4c1b3a0d5ae6d8c01310898d5c108a991
dualic <officialdualic@gmail.com>
2026-06-27 14:37:32 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,13 @@
+module.exports = (client) => {
 
+    console.clear();
+
+    console.log("================================");
+
+    console.log(`Logged in as ${client.user.tag}`);
+
+    console.log("Discord Economy Bot is online.");
+
+    console.log("================================");
+
+};
