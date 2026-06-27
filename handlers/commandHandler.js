bddc89e43463908f0d6191e825bdec5433b1c6f4
dualic <officialdualic@gmail.com>
2026-06-27 14:37:21 -0400

    Update commandHandler.js

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -1,1 +1,28 @@
+const fs = require("fs");
+const path = require("path");
 
+module.exports = (client) => {
+
+    const commandsPath = path.join(__dirname, "../commands");
+
+    const folders = fs.readdirSync(commandsPath);
+
+    for (const folder of folders) {
+
+        const folderPath = path.join(commandsPath, folder);
+
+        const commandFiles = fs
+            .readdirSync(folderPath)
+            .filter(file => file.endsWith(".js"));
+
+        for (const file of commandFiles) {
+
+            const command = require(path.join(folderPath, file));
+
+            client.commands.set(command.data.name, command);
+
+        }
+
+    }
+
+};
